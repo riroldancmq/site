57a906e8e0a12692956fdbf420955bf3901190b4
riroldancmq <ricardojaroldan@gmail.com>
2026-08-22 19:43:09 -0300

    Agregar PWA: manifest, service worker e íconos

---
+++ sw.js
@@ -0,0 +1,49 @@
+const CACHE_NAME = 'vencimientos-v1';
+const PRECACHE = [
+    './',
+    './index.html',
+    './app.js',
+    './styles.css',
+    './manifest.webmanifest',
+    './icons/icon-192.png',
+    './icons/icon-512.png',
+    './icons/icon-maskable-192.png',
+    './icons/icon-maskable-512.png',
+    './icons/apple-touch-icon.png'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(PRECACHE))
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) =>
+            Promise.all(
+                keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+            )
+        ).then(() => self.clients.claim())
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    if (event.request.method !== 'GET') return;
+
+    event.respondWith(
+        caches.match(event.request).then((cached) => {
+            const network = fetch(event.request)
+                .then((response) => {
+                    if (response.ok && event.request.url.startsWith(self.location.origin)) {
+                        const clone = response.clone();
+                        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
+                    }
+                    return response;
+                })
+                .catch(() => cached);
+            return cached || network;
+        })
+    );
+});
